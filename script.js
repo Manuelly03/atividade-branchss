e260d9c71ece21c18a63283386b46ac10c8c9c01
Manuelly03 <fernandesmanuelly341@gmail.com>
2026-09-21 23:03:53 -0300

    feat: adiciona calculadora

--- script.js
+++ script.js
@@ -14,3 +14,12 @@ function diminuir() {
 function alternarTema() {
     document.body.classList.toggle("dark");
 }
+
+function somar() {
+    const numero1 = Number(document.getElementById("numero1").value);
+    const numero2 = Number(document.getElementById("numero2").value);
+
+    const resultado = numero1 + numero2;
+
+    document.getElementById("resultado").textContent = resultado;
+}
